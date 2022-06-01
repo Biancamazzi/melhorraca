6473ba686124d6a107a683565cb640fa610f264b
fvzica <76051745+fvzica@users.noreply.github.com>
2022-06-01 18:36:39 -0300

    routes question with index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :quizzes, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :questions, only: [:index]
 end
