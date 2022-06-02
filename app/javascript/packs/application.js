ce0c7a061231f9d8f0334b0ecae8482932ed81d3
fvzica <76051745+fvzica@users.noreply.github.com>
2022-06-02 20:35:19 -0300

    codigo da tatchi

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,15 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// require("bootstrap")
+require("@fortawesome/fontawesome-free")
 
+import { runSlides } from "./plugins/questions"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+document.addEventListener('turbolinks:load', () => {
+  runSlides()
+});
