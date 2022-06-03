956ab1afedf3baa2c3e6b318a1365e4fc3df22ad
fvzica <76051745+fvzica@users.noreply.github.com>
2022-06-03 09:43:12 -0300

    button rails (without js)

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :questions, only: [:index]
   post "questions/aprove", to: "questions#aprove"
+  post "questions/decline", to: "questions#decline"
 
 end
