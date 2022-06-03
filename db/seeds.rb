ce1414b314809f8d79f86e6ca15d03553fadb3a5
fvzica <76051745+fvzica@users.noreply.github.com>
2022-06-03 18:38:00 -0300

    seed do question com photo_url. funcionando

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
   Quiz.all.destroy_all
   Question.all.destroy_all
-  Quiz.create(user_id: User.first.id, description: "Quiz 1",)
+  Quiz.create(user_id: User.first.id, description: "Quiz 1")
   # Create - Questions
   Question.create(
   quiz_id: Quiz.first.id, description: "Voce pode tosar e/ou escovar o seu cachorro com que frequencia?", photourl: "https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80")
@@ -22,7 +22,7 @@
   quiz_id: Quiz.first.id, description: "Facilidade para treinamento é algo muito relevante pra voce?", photourl: "https://images.unsplash.com/photo-1508109742312-c7d531211d11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80")
   # true == >= (trainability_value)
   Question.create(
-  quiz_id: Quiz.first.id,  description: "É muito importante pra voce que o cachorro se adapte bem com outros cachorros e pessoas fora do circulo familar?"), photourl: "https://images.pexels.com/photos/10289623/pexels-photo-10289623.jpeg?cs=srgb&dl=pexels-amar-preciado-10289623.jpg&fm=jpg"
+  quiz_id: Quiz.first.id,  description: "É muito importante pra voce que o cachorro se adapte bem com outros cachorros e pessoas fora do circulo familar?", photourl: "https://images.pexels.com/photos/10289623/pexels-photo-10289623.jpeg?cs=srgb&dl=pexels-amar-preciado-10289623.jpg&fm=jpg")
   # true == >= 5 (demeanor_value)
   Question.create(
   quiz_id: Quiz.first.id,  description: "Voce gostaria de ter uma raca de pequena?", photourl:"https://i.postimg.cc/TP3RpwKg/jack.jpg" )
