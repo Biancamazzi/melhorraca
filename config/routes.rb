a0935335089f6390ebbb713c45d23e6a5284f29b
BiancaMazzi <bianca.mazzi@hotmail.com>
2022-06-11 12:47:23 -0300

    update style in results

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   resources :quizzes, only: [:create, :show]
-  resources :results, only: [:show]
+  resources :results, only: [:create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :questions, only: [:index, :show]
   post "questions/approve/:id", to: "questions#approve"
