cd33d6b2bd185c41771d0875f0591069bcb310d3
fvzica <76051745+fvzica@users.noreply.github.com>
2022-06-12 16:46:08 -0300

    filtro funcionando com os puts pra teste no terminal, necessario colocar a validacao se nao chegar em nenhum resoltado'

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -27,61 +27,87 @@ class ResultsController < ApplicationController
   end
 
   def filter
-    question1 = Answer.where(user_id: current_user.id).where(question_id: 1).last.answer
-    question2 = Answer.where(user_id: current_user.id).where(question_id: 2).last.answer
-    question3 = Answer.where(user_id: current_user.id).where(question_id: 3).last.answer
-    question4 = Answer.where(user_id: current_user.id).where(question_id: 4).last.answer
-    question5 = Answer.where(user_id: current_user.id).where(question_id: 5).last.answer
-    question6 = Answer.where(user_id: current_user.id).where(question_id: 6).last.answer
-    question7 = Answer.where(user_id: current_user.id).where(question_id: 7).last.answer
-    question8 = Answer.where(user_id: current_user.id).where(question_id: 8).last.answer
-    # question1 = Answer.where(user_id: 1).where(question_id: 1).last.answer
-    # question2 = Answer.where(user_id: 1).where(question_id: 2).last.answer
-    # question3 = Answer.where(user_id: 1).where(question_id: 3).last.answer
-    # question4 = Answer.where(user_id: 1).where(question_id: 4).last.answer
-    # question5 = Answer.where(user_id: 1).where(question_id: 5).last.answer
-    # question6 = Answer.where(user_id: 1).where(question_id: 6).last.answer
-    # question7 = Answer.where(user_id: 1).where(question_id: 7).last.answer
-    # question8 = Answer.where(user_id: 1).where(question_id: 8).last.answer
-    if question1 == true
-      result = Info.select {|i| i['grooming_frequency_value'] >= 0.5}
-    else
-      result = Info.select {|i| i['grooming_frequency_value'] < 0.5}
+    # question1 = Answer.where(user_id: current_user.id).where(question_id: 1).last.answer
+    # question2 = Answer.where(user_id: current_user.id).where(question_id: 2).last.answer
+    # question3 = Answer.where(user_id: current_user.id).where(question_id: 3).last.answer
+    # question4 = Answer.where(user_id: current_user.id).where(question_id: 4).last.answer
+    # question5 = Answer.where(user_id: current_user.id).where(question_id: 5).last.answer
+    # question6 = Answer.where(user_id: current_user.id).where(question_id: 6).last.answer
+    # question7 = Answer.where(user_id: current_user.id).where(question_id: 7).last.answer
+    # question8 = Answer.where(user_id: current_user.id).where(question_id: 8).last.answer
+    question1 = Answer.where(user_id: 1).where(question_id: 1).last.answer
+    question2 = Answer.where(user_id: 1).where(question_id: 2).last.answer
+    question3 = Answer.where(user_id: 1).where(question_id: 3).last.answer
+    question4 = Answer.where(user_id: 1).where(question_id: 4).last.answer
+    question5 = Answer.where(user_id: 1).where(question_id: 5).last.answer
+    question6 = Answer.where(user_id: 1).where(question_id: 6).last.answer
+    question7 = Answer.where(user_id: 1).where(question_id: 7).last.answer
+    question8 = Answer.where(user_id: 1).where(question_id: 8).last.answer
+     if question6 == true && question7 == true && question8 == true
+      result = Info.where(size:'pequeno').or(Info.where(size:'medio').or(Info.where(size:'grande')))
+      puts result.count
+     end
+
+    if question6 && question7 == true && question8 == false
+      result = Info.where(size:'pequeno').or(Info.where(size:'medio'))
+      puts result.count
     end
+
+    if question6 == false && question7 && question8 == true
+      result = Info.where(size:'medio').or(Info.where(size:'grande'))
+      puts result.count
+    end
+
+    if question6 == false && question7 == false && question8 == true
+      result = Info.where(size:'grande')
+      puts result.count
+    end
+
+    if question6 == false && question7 == true && question8 == false
+      result = Info.where(size:'medio')
+      puts result.count
+    end
+
+    if question6 == true && question7 == false && question8 == false
+       result = Info.where(size:'pequeno')
+      puts result.count
+    end
+
+    if question1 == true
+      result = result.select {|i| i['grooming_frequency_value'] >= 0.5}
+    else
+      result = result.select {|i| i['grooming_frequency_value'] < 0.5}
+    end
+    puts result.count
+
     if question2 == true
       result = result.select {|i| i['shedding_value'] <= 0.4}
     else
       result = result.select {|i| i['shedding_value'] > 0.4}
     end
+    puts result.count
+
     if question3 == true
       result = result.select {|i| i['energy_level_value'] >= 0.5}
     else
       result = result.select {|i| i['energy_level_value'] < 0.5}
     end
+    puts result.count
+
     if question4 == true
       result = result.select {|i| i['trainability_value'] >= 0.5}
     else
       result = result.select {|i| i['trainability_value'] < 0.5}
     end
+    puts result.count
+
     if question5 == true
       result = result.select {|i| i['demeanor_value'] >= 0.5}
     else
       result = result.select {|i| i['demeanor_value'] < 0.5}
     end
-    if question6 && question7 && question8 == true
-      result = result.select {|i| i['size'] == 'pequeno' && i['size'] == 'medio' && i['size'] == 'grande'}
-    elsif question6 && question7 == true && question8 == false
-      result = result.select {|i| i['size'] == 'pequeno' && i['size'] == 'medio'}
-    elsif question6 == false && question7 && question8 == true
-      result = result.select {|i| i['size'] == 'medio' && i['size'] == 'grande'}
-    elsif question6 == false && question7 == false && question8 == true
-      result = result.select {|i| i['size'] == 'grande'}
-    elsif question6 == false && question7 == true && question8 == false
-      result = result.select {|i| i['size'] == 'medio'}
-    elsif question6 == true && question7 == false && question8 == false
-      result = result.select {|i| i['size'] == 'pequeno'}
-    end
-    @result = result
+    puts result.count
+    puts result.sample.id
   end
 
 
