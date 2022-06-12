50939dbdf0a62ef5a0d1cc46eb7f914fd6bcc140
fvzica <76051745+fvzica@users.noreply.github.com>
2022-06-12 18:28:17 -0300

    ajustes nas perguntas. necessario ajustar se nao teve nenhum resultado

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -35,14 +35,14 @@ class ResultsController < ApplicationController
     question6 = Answer.where(user_id: current_user.id).where(question_id: 6).last.answer
     question7 = Answer.where(user_id: current_user.id).where(question_id: 7).last.answer
     question8 = Answer.where(user_id: current_user.id).where(question_id: 8).last.answer
-    # question1 = Answer.where(user_id: 2).where(question_id: 1).last.answer
-    # question2 = Answer.where(user_id: 2).where(question_id: 2).last.answer
-    # question3 = Answer.where(user_id: 2).where(question_id: 3).last.answer
-    # question4 = Answer.where(user_id: 2).where(question_id: 4).last.answer
-    # question5 = Answer.where(user_id: 2).where(question_id: 5).last.answer
-    # question8 = Answer.where(user_id: 2).where(question_id: 8).last.answer
-    # question6 = Answer.where(user_id: 2).where(question_id: 6).last.answer
-    # question7 = Answer.where(user_id: 2).where(question_id: 7).last.answer
+    question1 = Answer.where(user_id: 2).where(question_id: 1).last.answer
+    question2 = Answer.where(user_id: 2).where(question_id: 2).last.answer
+    question3 = Answer.where(user_id: 2).where(question_id: 3).last.answer
+    question4 = Answer.where(user_id: 2).where(question_id: 4).last.answer
+    question5 = Answer.where(user_id: 2).where(question_id: 5).last.answer
+    question8 = Answer.where(user_id: 2).where(question_id: 8).last.answer
+    question6 = Answer.where(user_id: 2).where(question_id: 6).last.answer
+    question7 = Answer.where(user_id: 2).where(question_id: 7).last.answer
      if question6 == true && question7 == true && question8 == true
       result = Info.where(size:'pequeno').or(Info.where(size:'medio').or(Info.where(size:'grande')))
       puts result.count
@@ -74,35 +74,35 @@ class ResultsController < ApplicationController
     end
 
     if question1 == true
-      result = result.select {|i| i['grooming_frequency_value'] >= 0.1}
+      result = result.select {|i| i['grooming_frequency_value'] > 0}
     else
       result = result.select {|i| i['grooming_frequency_value'] < 0.5}
     end
     puts result.count
 
     if question2 == true
-      result = result.select {|i| i['shedding_value'] <= 0.4}
+      result = result.select {|i| i['shedding_value'] < 0.5}
     else
-      result = result.select {|i| i['shedding_value'] >= 0.1}
+      result = result.select {|i| i['shedding_value'] > 0}
     end
     puts result.count
 
     if question3 == true
-      result = result.select {|i| i['energy_level_value'] >= 0.1}
+      result = result.select {|i| i['energy_level_value'] > 0}
     else
       result = result.select {|i| i['energy_level_value'] < 0.5}
     end
     puts result.count
 
     if question4 == true
-      result = result.select {|i| i['trainability_value'] >= 0.5}
+      result = result.select {|i| i['trainability_value'] > 0.4}
     else
       result = result.select {|i| i['trainability_value'] < 0.5}
     end
     puts result.count
 
     if question5 == true
-      result = result.select {|i| i['demeanor_value'] >= 0.5}
+      result = result.select {|i| i['demeanor_value'] > 0.5}
     else
       result = result.select {|i| i['demeanor_value'] < 1.0}
     end
