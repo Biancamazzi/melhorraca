656f4ba926469bda5b1e07c1db3946ce32910ce3
fvzica <76051745+fvzica@users.noreply.github.com>
2022-06-14 21:11:07 -0300

    ajustes com redirect e validacao. retornando para inicio das questions quando n tem results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -12,6 +12,8 @@ class ResultsController < ApplicationController
 
     if @result.save
       redirect_to result_path(@result)
+    else
+      redirect_to questions_path, danger: "Nenhum resultado encontrado. Tente novamente"
     end
 
   end
@@ -39,77 +41,89 @@ class ResultsController < ApplicationController
     # question8 = Answer.where(user_id: 2).where(question_id: 8).last.answer
     # question6 = Answer.where(user_id: 2).where(question_id: 6).last.answer
     # question7 = Answer.where(user_id: 2).where(question_id: 7).last.answer
+    if question6 == false && question7 == false && question8 == false
+      return @filtrado = nil
+    end
+
      if question6 == true && question7 == true && question8 == true
       result = Info.where(size:'pequeno').or(Info.where(size:'medio').or(Info.where(size:'grande')))
-      # puts result.count
+      puts result.count
      end
 
     if question6 && question7 == true && question8 == false
       result = Info.where(size:'pequeno').or(Info.where(size:'medio'))
-      # puts result.count
+      puts result.count
     end
 
     if question6 == false && question7 && question8 == true
       result = Info.where(size:'medio').or(Info.where(size:'grande'))
-      # puts result.count
+      puts result.count
     end
 
     if question6 == false && question7 == false && question8 == true
       result = Info.where(size:'grande')
-      # puts result.count
+      puts result.count
     end
 
     if question6 == false && question7 == true && question8 == false
       result = Info.where(size:'medio')
-      # puts result.count
+      puts result.count
     end
 
     if question6 == true && question7 == false && question8 == false
        result = Info.where(size:'pequeno')
-      # puts result.count
+      puts result.count
     end
 
-      if question6 == false && question7 == false && question8 == false
-      redirect_to questions_path, danger: "Nenhum resultado encontrado. Tente novamente" and return
-    end
+
+
 
     if question1 == true
       result = result.select {|i| i['grooming_frequency_value'] > 0}
     else
-      result = result.select {|i| i['grooming_frequency_value'] < 0.5}
+      result = result.select {|i| i['grooming_frequency_value'] < 0.3}
     end
-    # puts result.count
+    puts result.count
+
+
 
     if question2 == true
-      result = result.select {|i| i['shedding_value'] < 0.5}
+      result = result.select {|i| i['shedding_value'] > 0.5}
     else
       result = result.select {|i| i['shedding_value'] > 0}
     end
-    # puts result.count
+    puts result.count
+
+
 
     if question3 == true
       result = result.select {|i| i['energy_level_value'] > 0}
     else
       result = result.select {|i| i['energy_level_value'] < 0.5}
     end
-    # puts result.count
+    puts result.count
+
+
 
     if question4 == true
       result = result.select {|i| i['trainability_value'] > 0.4}
     else
       result = result.select {|i| i['trainability_value'] < 0.5}
     end
-    # puts result.count
+    puts result.count
+
+
 
     if question5 == true
       result = result.select {|i| i['demeanor_value'] > 0.5}
     else
-      result = result.select {|i| i['demeanor_value'] < 1.0}
+      result = result.select {|i| i['demeanor_value'] < 1.1}
     end
-    # puts result.count
-    puts result.sample.id
+    puts result.count
+
+
     @filtrado = result.sample
+    end
+
   end
 
-
-end
