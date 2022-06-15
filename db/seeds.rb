8752dd4b28bdc2519ef517fd42ce673f5a5a5b25
BiancaMazzi <bianca.mazzi@hotmail.com>
2022-06-15 10:47:39 -0300

    update some writting details

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,16 @@ Quiz.all.destroy_all
   quiz_id: Quiz.first.id, description: "Can you grooming your dog frequently?", photourl: "https://images.unsplash.com/photo-1527526029430-319f10814151?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80")
   # true == >= 5 (grooming_frequency_value)
   Question.create(
-  quiz_id: Quiz.first.id, description: "Shedding its something that bothers you?", photourl: "https://photos.smugmug.com/photos/i-QKBF4L6/0/4K/i-QKBF4L6-4K.jpg")
+  quiz_id: Quiz.first.id, description: "Shedding is it something that bothers you?", photourl: "https://photos.smugmug.com/photos/i-QKBF4L6/0/4K/i-QKBF4L6-4K.jpg")
   # true == <= 4 (shedding_value)
   Question.create(
    quiz_id: Quiz.first.id, description: "Do you have time and energy to play with your new friend?", photourl: "https://images.unsplash.com/photo-1525253086316-d0c936c814f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" )
   # true == >= 5 (energy_level_value)
   Question.create(
-  quiz_id: Quiz.first.id, description: "Trainability its something that really matters to you?", photourl: "https://images.unsplash.com/photo-1508109742312-c7d531211d11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80")
+  quiz_id: Quiz.first.id, description: "Trainability is it something that really matters to you?", photourl: "https://images.unsplash.com/photo-1508109742312-c7d531211d11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80")
   # true == >= (trainability_value)
   Question.create(
-  quiz_id: Quiz.first.id,  description: "Its important to you that your dog accept other animals and strangers?", photourl: "https://images.pexels.com/photos/4588015/pexels-photo-4588015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+  quiz_id: Quiz.first.id,  description: "Is it important to you that your dog accept other animals and strangers?", photourl: "https://images.pexels.com/photos/4588015/pexels-photo-4588015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
   # true == >= 5 (demeanor_value)
   Question.create(
   quiz_id: Quiz.first.id,  description: "Would you like to have a small dog?", photourl:"https://i.postimg.cc/TP3RpwKg/jack.jpg" )
