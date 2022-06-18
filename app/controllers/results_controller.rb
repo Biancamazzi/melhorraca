26467018d76aac2c7056bf3401b5ca8b2625e890
fvzica <76051745+fvzica@users.noreply.github.com>
2022-06-18 10:06:09 -0300

    refatorando result

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -41,113 +41,50 @@ class ResultsController < ApplicationController
     # question8 = Answer.where(user_id: 2).where(question_id: 8).last.answer
     # question6 = Answer.where(user_id: 2).where(question_id: 6).last.answer
     # question7 = Answer.where(user_id: 2).where(question_id: 7).last.answer
-    if question6 == false && question7 == false && question8 == false
+    if !question6 && !question7 && !question8
       return @filtrado = nil
     end
+     a = []
+     a << 'pequeno' if question6
+     a << 'medio' if question7
+     a << 'grande' if question8
+     result = Info.where(size: a)
 
-     if question6 == true && question7 == true && question8 == true
-      result = Info.where(size:'pequeno').or(Info.where(size:'medio').or(Info.where(size:'grande')))
-
-     end
-
-    if question6 && question7 == true && question8 == false
-      result = Info.where(size:'pequeno').or(Info.where(size:'medio'))
-
-    end
-
-    if question6 == false && question7 && question8 == true
-      result = Info.where(size:'medio').or(Info.where(size:'grande'))
-
-    end
-
-    if question6 == false && question7 == false && question8 == true
-      result = Info.where(size:'grande')
-
-    end
-
-      if question6 == true && question7 == false && question8 == true
-      result = Info.where(size:'grande').or(Info.where(size:'grande'))
-
-    end
-
-    if question6 == false && question7 == true && question8 == false
-      result = Info.where(size:'medio')
-
-    end
-
-    if question6 == true && question7 == false && question8 == false
-       result = Info.where(size:'pequeno')
-
-    end
-
-
-
-    if question1 == true
-      result1 = result.select {|i| i['grooming_frequency_value'] > 0.4}
+    if question1
+      filtered = result.select {|i| i['grooming_frequency_value'] > 0.4}
     else
-      result1 = result.select {|i| i['grooming_frequency_value'] < 0.5}
+      filtered = result.select {|i| i['grooming_frequency_value'] < 0.5}
     end
+    result = filtered if filtered.size > 0
 
-     puts result1.count
-
-
-    if question2 == true
-      result2 = result1.select {|i| i['shedding_value'] > 0.5}
+    if question2
+      filtered = result.select {|i| i['shedding_value'] > 0.5}
     else
-      result2 = result1.select {|i| i['shedding_value'] > 0}
+      filtered = result.select {|i| i['shedding_value'] > 0}
     end
+    result = filtered if filtered.size > 0
 
-      puts result2.count
-
-
-
-    if question3 == true
-      result3 = result2.select {|i| i['energy_level_value'] > 0}
+    if question3
+      filtered = result.select {|i| i['energy_level_value'] > 0}
     else
-      result3 = result2.select {|i| i['energy_level_value'] < 0.5}
+      filtered = result.select {|i| i['energy_level_value'] < 0.5}
     end
+    result = filtered if filtered.size > 0
 
-      puts result3.count
-
-
-    if question4 == true
-      result4 = result3.select {|i| i['trainability_value'] > 0.4}
+    if question4
+      filtered = result.select {|i| i['trainability_value'] > 0.4}
     else
-      result4 = result3.select {|i| i['trainability_value'] < 0.5}
+      filtered = result.select {|i| i['trainability_value'] < 0.5}
     end
+    result = filtered if filtered.size > 0
 
-      puts result4.count
-
-
-
-
-    if question5 == true
-      result5 = result4.select {|i| i['demeanor_value'] > 0.4}
+    if question5
+      filtered = result.select {|i| i['demeanor_value'] > 0.4}
     else
-      result5 = result4.select {|i| i['demeanor_value'] < 0.5}
+      filtered = result.select {|i| i['demeanor_value'] < 0.5}
     end
-
-       puts result5.count
-
-
-
-
-    if result1.count == 0 || result1.count == nil
-            return @filtrado = result.sample
-        elsif result2.count == 0 || result2.count == nil
-            return @filtrado = result1.sample
-       elsif result3.count == 0 || result3.count == nil
-           return @filtrado = result2.sample
-         elsif result4.count == 0 || result4.count == nil
-           return @filtrado = result3.sample
-         elsif result5.count == 0 || result5.count == nil
-           return @filtrado = result4.sample
-    end
-
-    @filtrado = result5.sample
-
-    end
-
-
+    result = filtered if filtered.size > 0
+    @filtrado = result.sample
   end
 
+    end
